b84b434c4b8ce4fcf039ebc749be508ea09fa710
hoyama <ghettomoon00@gmail.com>
2020-10-31 11:36:22 +0900

    app.blade.php

---
+++ server/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>めしナビ - @yield('title')</title>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>    <div class="container">
+@yield('content')
+    </div>  
+</body>
+</html>
